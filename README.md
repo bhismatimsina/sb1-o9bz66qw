845082fd81ab8c5aff49a47a82e2e700f56c551c
bhismatimsina <hello@bhismatimsina.com.np>
2024-12-26 12:46:41 +0545

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# sb1-o9bz66qw
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/bhismatimsina/sb1-o9bz66qw)
---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+        <h1 className="text-3xl font-bold text-gray-900">
+          Welcome to My Cool Project
+        </h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
